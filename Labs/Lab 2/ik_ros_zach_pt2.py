67270608c97d41ce6cb71656fa46233679f184e5
Zach Kushnir <zskushnir@gmail.com>
2026-02-15 22:33:03 -0500

    updated zach

--- Labs/Lab 2/ik_ros_zach_pt2.py
+++ Labs/Lab 2/ik_ros_zach_pt2.py
@@ -515,35 +515,35 @@ def main():
              ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi / 2)),
 
             ("Zigzag LEFT",
-             [0.2, -0.5, 0.1],
+             [0.2, .2, 0.1],
              ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi / 2)),
 
             # 
             ("Reach UP",
-             [-0.5, -0.3, 0.2],
+             [-0.5, 0, 0.2],
              ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi / 2)),
 
             # 
             ("Reach FORWARD LOW",
-             [0.4, -0.3, -0.1],
+             [-.1, 0, -0.1],
              ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi / 2)),
 
              ("Zigzag RIGHT",
-             [0.5, -0.2, 0.2],
+             [0.2, 0.1, 0.2],
              ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi / 2)),
 
             ("Zigzag LEFT",
-             [0.1, 0.5, 0.1],
+             [-0.1, 0.2, 0.1],
              ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi / 2)),
 
             # 
             ("Reach UP",
-             [-0.4, -0.3, 0.2],
+             [0.2, 0, 0.2],
              ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi / 2)),
 
             # 
             ("Reach FORWARD LOW",
-             [0.4, -0.3, -0.3],
+             [0.0, 0.2, -0.3],
              ikpy.utils.geometry.rpy_matrix(0.0, 0.0, -np.pi / 2)),
         ]
 
